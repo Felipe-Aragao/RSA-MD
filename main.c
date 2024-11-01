2d64c0fa0774297a26ea8b35f29daa2b255a9621
Felipe Aragão <lipelima2006@gmail.com>
2024-11-01 17:44:03 -0300

    divisão em etapas

--- main.c
+++ main.c
@@ -5,16 +5,102 @@
 
 typedef unsigned long long ull;
 
+void chave();
 ull criptografar(ull letra, ull e, ull n);
 ull descriptografar(ull codigo, ull d, ull n);
 ull mod_multiplicao(ull c, ull d, ull n);
 long long euclidesEstendido(long long a, long long b);
 
+
+//FIXME: Está dando erro para números pequenos, provavelmente o problema e na parte da descriptografia
 int main()
 {   
-    //Entrada da mesnsagem pelo usúario
+    int modo;
+    printf("1 - Gerar chave pública\n2 - Encriptar\n3 - Desencriptar\n");
+    scanf("%d", &modo);
+
+    if (modo > 3 || modo < 1)
+    {
+        printf("Escolha uma opção válida");
+        return 1;
+    }
+
+    ull p, q, e, n;
+    
+    // Gerar chave pública
+    if (modo == 1)
+    {
+        chave();
+    }
+    
+    // Encriptar
+    if (modo == 2)
+    {
+        //Entrada da mesnsagem pelo usúario
+        char menssagem[MAX];
+        printf("Menssagem: ");
+        //TODO Mudar para fgets
+        getchar();
+        gets(menssagem);
+        int tamanho = strlen(menssagem);
+
+        printf("e: ");
+        scanf("%llu", &e);
+        printf("n: ");
+        scanf("%llu", &n);
+
+        FILE *f;
+        f = fopen("encriptado.txt", "w");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            fprintf(f, "%llu ", criptografar((int) menssagem[i], e, n));
+        }
+
+        fclose(f);
+    }
+
+    //Desencriptar
+    if (modo == 3)
+    {
+        printf("P: ");
+        scanf("%llu", &p);
+        printf("Q: ");
+        scanf("%llu", &q);
+        printf("e: ");
+        scanf("%llu", &e);
+
+        FILE *cript;
+        FILE *descript;
+        cript = fopen("encriptado.txt", "r");
+        descript = fopen("desencriptado.txt", "w");
+
+        //Achar o totiente (phi)
+        ull totiente = (p-1)*(q-1);
+
+        // Euclides estendido para achar s de e(mod totiente)
+        long long inverso = euclidesEstendido(e, totiente);
+        //Garantir que o s é positivo, portanto o inverso
+        while (inverso < 0)
+        {
+            inverso += totiente;
+        } 
+        n = p * q;
+
+        ull codificada;
+        while (fscanf(cript, "%llu", &codificada) != EOF)
+        {
+            printf("%llu ", codificada);
+            fprintf(descript, "%c", descriptografar(codificada, inverso, n));
+        }
+        fclose(cript);
+        fclose(descript);
+    }
+
+/*     //Entrada da mesnsagem pelo usúario
     char menssagem[MAX];
     printf("Menssagem: ");
+    getchar();
     gets(menssagem);
 
     int tamanho = strlen(menssagem);
@@ -22,15 +108,11 @@ int main()
 
     //p e q devem ser numeros primos
     //TODO: Confirmar que p e q são primos
-    ull p = 999863;
-    ull q = 999671;
-
-/*     printf("P: ");
-    printf("Q: ");
-    scanf("%d%d", &p, q); */
+    p = 999863;
+    q = 999671;
     
     //modular
-    ull n = p*q;
+    n = p*q;
     
     //Achar o totiente (phi)
     ull totiente = (p-1)*(q-1);
@@ -38,9 +120,8 @@ int main()
     
     //e e totiente devem ser primos entre si.
     //TODO: Confirmar que e e totiente são primos entre si
-    int e;
     printf("e: ");
-    scanf("%d", &e);
+    scanf("%llu", &e);
 
     //Criptografar
     ull codificada[MAX];
@@ -74,7 +155,27 @@ int main()
     for (int i = 0; i < tamanho; i++)
     {
         printf("%c", decodificar[i]);
-    }
+    } */
+}
+
+void chave()
+{
+    ull p, q, e, n;
+
+    FILE *f;
+    f = fopen("publica.txt", "w");
+
+    printf("P: ");
+    scanf("%llu", &p);
+    printf("Q: ");
+    scanf("%llu", &q);
+    printf("e: ");
+    scanf("%llu", &e);
+
+    n = p*q;
+
+    fprintf(f, "%llu %llu", e, n);
+    fclose(f);
 }
 
 ull criptografar(ull letra, ull e, ull n)
@@ -85,7 +186,6 @@ ull criptografar(ull letra, ull e, ull n)
         //Exponenciacao modular
         resultado = (resultado * letra) % n;
     }
-    printf("%llu ", resultado);
 
     return resultado;
 }
